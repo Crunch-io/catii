a755f394bf5aacfe57d2b5ca1982632f79b73680
Robert Brewer <fumanchu@aminus.org>
2023-03-06 17:59:41 -0800

    More benchmarks

--- benchmarks/test_ccubes.py
+++ benchmarks/test_ccubes.py
@@ -13,9 +13,36 @@ class Test_ccube_1d(UnitBenchmark):
     def test_ccube_1d_count(self):
         self._ccube_1d("count", ffuncs.ffunc_count(), 1)
 
+    def test_ccube_1d_count_wt(self):
+        W = self.numeric()
+        self._ccube_1d("count.wt", ffuncs.ffunc_count(weights=W), 8)
+
     def test_ccube_1d_valid_count(self):
         A = self.categorical(indexed=False)
         self._ccube_1d("valid_count", ffuncs.ffunc_valid_count(A), 10)
+
+    def test_ccube_1d_valid_count_wt(self):
+        A = self.categorical(indexed=False)
+        W = self.numeric()
+        self._ccube_1d("valid_count.wt", ffuncs.ffunc_valid_count(A, weights=W), 12)
+
+    def test_ccube_1d_sum(self):
+        N = self.numeric()
+        self._ccube_1d("sum", ffuncs.ffunc_sum(N), 12)
+
+    def test_ccube_1d_sum_wt(self):
+        N = self.numeric()
+        W = self.numeric()
+        self._ccube_1d("sum.wt", ffuncs.ffunc_sum(N, weights=W), 12)
+
+    def test_ccube_1d_mean(self):
+        N = self.numeric()
+        self._ccube_1d("mean", ffuncs.ffunc_mean(N), 16)
+
+    def test_ccube_1d_mean_wt(self):
+        N = self.numeric()
+        W = self.numeric()
+        self._ccube_1d("mean.wt", ffuncs.ffunc_mean(N, weights=W), 15)
 
 
 class Test_ccube_1d_x_1d(UnitBenchmark):
@@ -30,7 +57,7 @@ class Test_ccube_1d_x_1d(UnitBenchmark):
 
     def test_ccube_1d_x_1d_valid_count(self):
         A = self.categorical(indexed=False)
-        self._ccube_1d_x_1d("valid_count", ffuncs.ffunc_valid_count(A), 40)
+        self._ccube_1d_x_1d("valid_count", ffuncs.ffunc_valid_count(A), 45)
 
 
 class Test_ccube_2d(UnitBenchmark):
--- benchmarks/test_xcubes.py
+++ benchmarks/test_xcubes.py
@@ -13,9 +13,36 @@ class Test_xcube_1d(UnitBenchmark):
     def test_xcube_1d_count(self):
         self._xcube_1d("count", xfuncs.xfunc_count(), 8)
 
+    def test_xcube_1d_count_wt(self):
+        W = self.numeric()
+        self._xcube_1d("count.wt", xfuncs.xfunc_count(weights=W), 16)
+
     def test_xcube_1d_valid_count(self):
         A = self.categorical(indexed=False)
         self._xcube_1d("valid_count", xfuncs.xfunc_valid_count(A), 22)
+
+    def test_xcube_1d_valid_count_wt(self):
+        A = self.categorical(indexed=False)
+        W = self.numeric()
+        self._xcube_1d("valid_count.wt", xfuncs.xfunc_valid_count(A, weights=W), 16)
+
+    def test_xcube_1d_sum(self):
+        N = self.numeric()
+        self._xcube_1d("sum", xfuncs.xfunc_sum(N), 15)
+
+    def test_xcube_1d_sum_wt(self):
+        N = self.numeric()
+        W = self.numeric()
+        self._xcube_1d("sum.wt", xfuncs.xfunc_sum(N, weights=W), 15)
+
+    def test_xcube_1d_mean(self):
+        N = self.numeric()
+        self._xcube_1d("mean", xfuncs.xfunc_mean(N), 25)
+
+    def test_xcube_1d_mean_wt(self):
+        N = self.numeric()
+        W = self.numeric()
+        self._xcube_1d("mean.wt", xfuncs.xfunc_mean(N, weights=W), 20)
 
 
 class Test_xcube_1d_x_1d(UnitBenchmark):
@@ -30,7 +57,7 @@ class Test_xcube_1d_x_1d(UnitBenchmark):
 
     def test_xcube_1d_x_1d_valid_count(self):
         A = self.categorical(indexed=False)
-        self._xcube_1d_x_1d("valid_count", xfuncs.xfunc_valid_count(A), 20)
+        self._xcube_1d_x_1d("valid_count", xfuncs.xfunc_valid_count(A), 22)
 
 
 class Test_xcube_2d(UnitBenchmark):
