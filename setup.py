557d01cac73faf9475da6fccd629793a33910332
Robert Brewer <fumanchu@aminus.org>
2023-04-06 09:20:18 -0700

    1.0.0a11

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="catii",
-    version="1.0.0a10",
+    version="1.0.0a11",
     author="Robert Brewer, Slobodan Ilić",
     author_email="dev@crunch.io",
     description="A library for N-dimensional categorical data.",
