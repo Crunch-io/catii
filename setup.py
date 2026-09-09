61fb5a586a7a5791535fb23a637f65ececa07b26
Slobodan Ilic <slobodan.a.ilic@gmail.com>
2026-09-09 11:09:42 +0200

    Bump version to 1.0.5.dev1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="catii",
-    version="1.0.4",
+    version="1.0.5.dev1",
     author="Robert Brewer, Slobodan Ilić",
     author_email="dev@crunch.io",
     description="A library for N-dimensional categorical data.",
